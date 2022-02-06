c9de87d2be54496430b57f43eb0d781b1744ab58
smkoontz1 <smkoontz1@gmail.com>
2022-02-06 13:22:15 -0600

    Builds with lincoln server and shared

---
+++ BlazorWasmMicroservicesPoc/Lincoln/Server/Controllers/LincolnController.cs
@@ -0,0 +1,25 @@
+using Lincoln.Shared.V1;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+using WeatherForecasts.Shared;
+
+namespace Lincoln.Server.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    [RequiredScope("Lincoln.Read")]
+    public class LincolnController : ControllerBase
+    {
+        [HttpGet("/lincoln")]
+        public LincolnForecastResponse Get()
+        {
+            return new LincolnForecastResponse
+            {
+                Location = "Lincoln",
+                WeatherType = WeatherType.Chilly
+            };
+        }
+    }
+}
---
+++ BlazorWasmMicroservicesPoc/Lincoln/Shared/V1/LincolnForecastResponse.cs
@@ -0,0 +1,10 @@
+﻿using WeatherForecasts.Shared;
+
+namespace Lincoln.Shared.V1
+{
+    public class LincolnForecastResponse
+    {
+        public string? Location { get; set; }
+        public WeatherType WeatherType { get; set; }
+    }
+}
